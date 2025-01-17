49c3c7155dc20f5324b7a7b855581bd0ee167077
osundstrom <oskar.sundstrom123@gmail.com>
2025-01-17 01:26:56 +0100

    kommnetarer i auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,25 +1,27 @@
-const jwt = require("jsonwebtoken");
 
+
+const jwt = require("jsonwebtoken"); //jwt
+
+//funktion för autensiering
 const authenticate = (ctx, next) => {
-    const token = ctx.headers.authorization?.split(" ")[1];  
+    const token = ctx.headers.authorization?.split(" ")[1]; //token ifrån header
 
-    if (!token) {
+    if (!token) { //om ingen token
         ctx.status = 400;
-        ctx.body = { error: "Ingen token hittad" };
+        ctx.body = { error: "Ingen token hittad" }; //meddelande
         return;
     }
 
     try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY); //veriferar med JWT secret key.
+        ctx.state.user = decoded;  //sparas i ctx.state
         
-        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-        
-        ctx.state.user = decoded;  
-        
-        return next(); 
+        return next(); //fortsätt till nästa middelwere
     } catch (error) {
         ctx.status = 400;
-        ctx.body = { error: "ogiltig" };
+        ctx.body = {error: "ogiltig"};
     }
 };
 
+//exporterar
 module.exports = authenticate;
